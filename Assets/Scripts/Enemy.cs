0f14619056c7fd47b11007c3e19848bf21fc9b42
Dustin Cargile <dcargile84@gmail.com>
2022-11-28 21:27:09 -0600

    Added Coroutine to Enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,24 +9,49 @@ public class Enemy : MonoBehaviour
     [SerializeField]
     private float _upperbound = 6.5f, 
                   _lowerbound = -6.5f;
-    
 
+    [SerializeField]
+    private float _timeDelay = 0.3f;
     private float _xPos;
+
+    private float _leftbound = -9.5f,
+                  _rightbound = 9.5f;
     // Start is called before the first frame update
     void Start()
     {
          getNewPos();
+        //StartCoroutine(moveRoutine());
     }
 
     // Update is called once per frame
     void Update()
     {
-      transform.Translate(-Vector3.up * _speed * Time.deltaTime);
-
-        if (transform.position.y < _lowerbound) 
+        MoveDown();
+    }
+    private void MoveLeft() 
+    {
+        transform.Translate(-Vector3.left * _speed * Time.deltaTime);
+        if (Mathf.Abs(transform.position.x) > _rightbound) 
         {
             getNewPos();
         }
+    }
+    private void MoveRight()
+    {
+        transform.Translate(-Vector3.right * _speed * Time.deltaTime);
+        if (Mathf.Abs(transform.position.x) > _rightbound)
+        {
+            getNewPos();
+        }
+    }
+    private void MoveDown() 
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        if (transform.position.y < _lowerbound)
+        {
+            getNewPos();
+        }
+
     }
     private void getNewPos() 
     {
@@ -42,6 +67,18 @@ public class Enemy : MonoBehaviour
         if (other.tag == "Player") 
         {
             Destroy(gameObject);
+        }
+    }
+
+    IEnumerator moveRoutine() 
+    {
+        while (true) 
+        {
+            MoveDown();
+            yield return new WaitForSeconds(_timeDelay);
+            MoveLeft();
+            yield return new WaitForSeconds(_timeDelay);
+            MoveRight();
         }
     }
 }
